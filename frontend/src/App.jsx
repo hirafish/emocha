de19b2c0c22cb00064ee7f970c501c610fde3ba9
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-10 11:08:29 +0900

    Settingsページを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { FetchFromPython } from './api/Sample';
 import { ConnectSocketIo } from './socketio/Socketio';
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 function App() {
   // FastAPIとの通信
@@ -28,7 +29,8 @@ function App() {
       <div className='flex h-full'>
         <Navigation />
         <div className="flex-1 bg-gray-100 w-full h-full">
-          <Home />
+          {/* <Home /> */}
+          <Settings />
         </div>
       </div>
     </div>
