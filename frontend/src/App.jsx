888ff96b6fd7cdfd1afcc22c4281f0a087f00e83
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-10 11:01:47 +0900

    Homeページを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { FetchFromPython } from './api/Sample';
 import { ConnectSocketIo } from './socketio/Socketio';
 import Navigation from './components/Navigation/Navigation';
+import Home from './pages/Home';
 
 function App() {
   // FastAPIとの通信
@@ -26,6 +27,9 @@ function App() {
     <div className="w-full h-screen">
       <div className='flex h-full'>
         <Navigation />
+        <div className="flex-1 bg-gray-100 w-full h-full">
+          <Home />
+        </div>
       </div>
     </div>
   );
