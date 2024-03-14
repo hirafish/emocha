037dab9180086b60f804b52db45983cab3b88894
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-14 17:25:56 +0900

    roomカードのデータをグローバル値に置き換え

--- frontend/src/components/main/chat/ChatRoomList.jsx
+++ frontend/src/components/main/chat/ChatRoomList.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 
 const ChatRoomList=()=>{
     const {roomDataList}=useContext(RoomDataListContext);
-    console.log(Object.keys(roomDataList))
+    console.log(roomDataList)
     // 今のルーム　border-l-4 border-red-500
     return (
     <div className="sidebar hidden lg:flex w-1/3 flex-2 flex-col pr-6">
@@ -12,6 +12,13 @@ const ChatRoomList=()=>{
             <input type="text" className="outline-none py-2 block w-full bg-transparent border-b-2 border-gray-200" placeholder="Search" />
         </div>
         <div className="flex-1 h-full overflow-auto px-2">
+            {roomDataList.map((roomData,index)=>{
+                return (
+                    <span key={index}>
+                        <RoomCard iconImage={roomData.icon.image} iconColor={roomData.icon.color} roomName={roomData.name} />
+                    </span>
+                )
+            })}
             <RoomCard iconImage={"Otter"} iconColor={"Pink"} roomName={"For All"} />
             <RoomCard iconImage={"Otter"} iconColor={"Pink"} roomName={"Room 1"} />
         </div>
