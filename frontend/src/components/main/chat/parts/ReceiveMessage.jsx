5ac5a65130a3ee33975a59cc380c920a1827a48b
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-15 17:34:08 +0900

    スラングの表示・非表示を追加

--- frontend/src/components/main/chat/parts/ReceiveMessage.jsx
+++ frontend/src/components/main/chat/parts/ReceiveMessage.jsx
@@ -1,11 +1,27 @@
 import UserIcon from "../../globalParts/UserIcon";
 import DisplayEmojis from "./DisplayEmojis";
 import Tooltip from "../../globalParts/Tooltip";
+import { useState } from "react";
 
 const ReceiveMessage=({receiveData})=>{
+    // iconTextをUseIconコンポーネントのprops型に変換
     const iconText=receiveData.iconText;
     const preUserIconData=iconText.slice(1,iconText.length-1)
     const userIconDataList=preUserIconData.split(";");
+    // ------------------------------
+
+    // スラングの情報
+
+    // firebaseを参照してスラングが含まれてないときは空配列を返す
+    const slangList=
+        [
+            {shortcodes:"💅",meaning:"I do't care"},
+            {shortcodes:"🥑",meaning:"basic"},
+            {shortcodes:"🧢",meaning:"lie"}
+        ];
+    
+    
+    
     return(
         <div className="message mb-4">
             <div className="flex">
@@ -17,9 +33,10 @@ const ReceiveMessage=({receiveData})=>{
                 </div>
             </div>
             <div className="relative group w-fit">
-                <Tooltip />
+                {slangList[0]?<Tooltip />:undefined}
+                {/* <Tooltip /> */}
                 <div className="flex-1 px-2 w-fit">
-                    <div className="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700 ml-6 border-2 border-gray-300 hover:border-white">
+                    <div className={slangList[0]?"inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700 ml-6 border-2 border-gray-300 hover:border-white":"inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700 ml-6 border-2 border-gray-300"}>
                         <span className="flex items-center flex-wrap overflow-auto">
                             <DisplayEmojis emojiShortcodesList={receiveData.message} />
                         </span>
--- frontend/src/components/main/globalParts/Tooltip.jsx
+++ frontend/src/components/main/globalParts/Tooltip.jsx
@@ -4,14 +4,12 @@ const Tooltip=()=>{
         {shortcodes:"🥑",meaning:"basic"},
         {shortcodes:"🧢",meaning:"lie"}
         ]
-    const colNam=slangList.length;
-    console.log(colNam)
     return(
         <>
             {slangList?
-            <div class="absolute top-[2.8em] flex flex-col hidden group-hover:flex w-[280px] ml-6">
-                <div class="w-3 h-3 -mb-2 rotate-45 bg-white opacity-70 ml-8"></div>
-                <span class="relative z-10 text-xs p-2 px-6 leading-none whitespace-no-wrap bg-white shadow-lg w-full rounded-full opacity-70">
+            <div className="absolute top-[2.8em] flex flex-col hidden group-hover:flex w-[280px] ml-6">
+                <div className="w-3 h-3 -mb-2 rotate-45 bg-white opacity-70 ml-8"></div>
+                <span className="relative z-10 text-xs p-2 px-6 leading-none whitespace-no-wrap bg-white shadow-lg w-full rounded-full opacity-70">
                     <span className="w-full flex flex-col justify-center items-center">
                         {slangList.map((slangObj,index)=>{
                             return(
