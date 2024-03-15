60019bc2cc5ec380d540a7536e572e46786a3f81
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-15 16:58:14 +0900

    ツールチップの位置を変更

--- frontend/src/components/main/chat/parts/SendMassage.jsx
+++ frontend/src/components/main/chat/parts/SendMassage.jsx
@@ -1,34 +1,6 @@
-import UserIcon from "../../globalParts/UserIcon";
 import DisplayEmojis from "./DisplayEmojis";
 
-// const TranslateFromIconTextToUserIcon=({iconText,iconSize})=>{
-//     const preUserIconData=iconText.slice(1,iconText.length-1)
-//     const userIconDataList=preUserIconData.split(";");
-//     return (
-//         <>
-//             <UserIcon image={userIconDataList[0]} color={userIconDataList[1]} size={iconSize} />
-//         </>
-//     )
-// }
-
-// const FormatMessage=({message})=>{
-//     const unformatMessage=message;
-//     const iconTextInMessage=message.match(/@[A-Z][a-z]+;[A-Z][a-z]+;/g);
-//     console.log(iconTextInMessage);
-    
-//     return (
-//         <>
-//         {iconTextInMessage?<TranslateFromIconTextToUserIcon iconText={iconTextInMessage[0]} iconSize={6} />:<>{message}</>}
-//         </>
-//     )
-// }
-
 const SendMessage=({message})=>{
-    // const iconTextInMessage=message.match(/@[A-Z][a-z]+;[A-Z][a-z]+;/g);
-    // if(!iconTextInMessage){
-    // }else{
-    // }
-
     return (
         <div className="message me mb-4 flex text-right">
             <div className="flex-1 px-2">
--- frontend/src/components/main/globalParts/Tooltip.jsx
+++ frontend/src/components/main/globalParts/Tooltip.jsx
@@ -9,8 +9,8 @@ const Tooltip=()=>{
     return(
         <>
             {slangList?
-            <div class="absolute top-[2.8em] flex flex-col items-center hidden group-hover:flex left-[12%] w-5/6">
-                <div class="w-3 h-3 -mb-2 rotate-45 bg-white opacity-70"></div>
+            <div class="absolute top-[2.8em] flex flex-col hidden group-hover:flex w-[280px] ml-6">
+                <div class="w-3 h-3 -mb-2 rotate-45 bg-white opacity-70 ml-8"></div>
                 <span class="relative z-10 text-xs p-2 px-6 leading-none whitespace-no-wrap bg-white shadow-lg w-full rounded-full opacity-70">
                     <span className="w-full flex flex-col justify-center items-center">
                         {slangList.map((slangObj,index)=>{
