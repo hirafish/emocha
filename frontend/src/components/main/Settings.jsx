c2969f014e31997046909e9b102c817eb0cba9d9
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-16 17:14:30 +0900

    設定ページのUIを変更

--- frontend/src/components/main/Settings.jsx
+++ frontend/src/components/main/Settings.jsx
@@ -57,14 +57,14 @@ const Settings=()=>{
 
     // Sendボタンをクリックした時の処理
     const handleClickSend=()=>{
-        const sendData=previews;
-        console.log(sendData);
+        const postData=previews;
+        console.log(postData);
         try{
             // ここでsendDataをバックエンドにpostする
             // 実行結果（true / false）をもらう
             const response=true; // 実行結果を格納する変数
             if(response===true){
-                setUserSettings(sendData);
+                setUserSettings(postData);
                 alert("設定を更新しました！")
             }else{
                 alert("現在、設定を更新することができません。")
@@ -82,56 +82,77 @@ const Settings=()=>{
             <div className="lg:block heading flex-2">
                 <h1 className="text-xl py-1 xl:text-3xl xl:text-gray-700 xl:mb-4 border-b-2 border-gray-200">Settings</h1>
             </div>
-            <div className="flex-1  h-full flex flex-col items-center justify-evenly">
-            <div className=" container flex flex-col items-center justify-center">
-                <div className="flex-1 container flex flex-col items-center justify-center my-8">
-                    <h2 className="text-xl py-1 mb-4 border-b-2 border-gray-200">Icon</h2>
-                    <p className='mb-1'>Preview</p>
-                    <div className="mb-4 w-20 h-20">
-                        <UserIcon image={previews.icon.image} color={previews.icon.color} size={20}/>
-                    </div>
-                    <div className="flex justify-evenly w-full max-w-xs mx-auto">
-                        <form className="w-full">
-                            <select value={previews.icon.image} onChange={handleChangeIconImage} id="iconImages" className="text-center bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-10 px-3 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                {Object.keys(iconsCatalog.image).map((image,index)=>{
-                                    return (
-                                        <option key={index} value={image}>{image}</option>
-                                    )
-                                })}
-                            </select>
-                        </form>
-                        <form className="w-full ml-2">
-                            <select value={previews.icon.color} onChange={handleChangeIconColor} id="iconColors" className="text-center bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-10 px-3 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                {Object.keys(iconsCatalog.color).map((color,index)=>{
-                                    return (
-                                        <option key={index} value={color}>{color}</option>
-                                    )
-                                })}
-                            </select>
-                        </form>
-                    </div>
-                </div>
-                <div className="flex-1 container flex flex-col items-center justify-center my-8 ">
-                    <h2 className="text-xl py-1 mb-4 border-b-2 border-gray-200">Language</h2>
-                    <div className="flex justify-evenly w-full max-w-xs mx-auto">
-                        <form className="w-full">
-                            <select value={previews.language} onChange={handleChangeLanguage} id="languages" className="text-center bg-white border border-neutral-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-10 px-3 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                {languagesCatalog.map((language,index)=>{
-                                    return (
-                                        <option key={index} value={language}>{language}</option>
-                                    )
-                                })}
-                            </select>
-                        </form>
-                    </div>      
-                </div>
-            </div>
-            <div className=" container flex justify-center items-center max-w-xs mx-auto">
-                <button onClick={handleClickSend} type="button" className="inline-flex items-center justify-center w-4/5 px-5 py-3 text-sm font-medium tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-md hover:bg-blue-700 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none">
-                    Save
-                </button>
+            <div className="flex-1  h-full overflow-auto">
+                <div className="mx-10">
+                    <div className="container mb-10">
+                        <div className="containe my-8 w-full">
+                            <div className="md:flex justify-center">
+                                <div className="flex flex-col justify-center items-center md:mr-12">
+                                    <p className='mb-1'>Preview</p>
+                                    <div className="mb-4 w-20 h-20">
+                                        <UserIcon image={previews.icon.image} color={previews.icon.color} size={20}/>
+                                    </div>
+                                </div>
+                                <div className="w-full grid gap-6 mb-6 md:grid-cols-2 items-center">
+                                    <div className=" md:w-3/4">
+                                        <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
+                                        <form>
+                                            <select value={previews.icon.image} onChange={handleChangeIconImage} id="iconImages" className="text-center bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                {Object.keys(iconsCatalog.image).map((image,index)=>{
+                                                    return (
+                                                        <option key={index} value={image}>{image}</option>
+                                                    )
+                                                })}
+                                            </select>
+                                        </form>
+                                    </div>
+                                    <div className="md:w-3/4">
+                                        <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Color</label>
+                                        <form className="w-full">
+                                            <select value={previews.icon.color} onChange={handleChangeIconColor} id="iconColors" className="text-center bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                {Object.keys(iconsCatalog.color).map((color,index)=>{
+                                                    return (
+                                                        <option key={index} value={color}>{color}</option>
+                                                    )
+                                                })}
+                                            </select>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-            </div>
+                        <div className="md:w-3/5 my-8">
+                            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User name</label>
+                            <input type="text" id="first_name" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
+                        <div className="md:w-3/5 my-8">
+                            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SNS URL</label>
+                            <input type="text" id="first_name" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
+                        
+                        <div className="container my-8">
+                            <div className="md:w-1/5 min-w-40">
+                            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Language</label>
+                                <form className="w-full">
+                                    <select value={previews.language} onChange={handleChangeLanguage} id="languages" className="text-center bg-white border border-neutral-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        {languagesCatalog.map((language,index)=>{
+                                            return (
+                                                <option key={index} value={language}>{language}</option>
+                                            )
+                                        })}
+                                    </select>
+                                </form>
+                            </div>      
+                        </div>
+                        
+                    </div>
+                    <div className=" container flex justify-center md:justify-start items-center mx-auto my-6">
+                        <button onClick={handleClickSend} type="button" className="inline-flex items-center justify-center w-36 px-5 py-3 text-sm font-medium tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-md hover:bg-blue-700 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:shadow-outline focus:outline-none">
+                            Save
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
