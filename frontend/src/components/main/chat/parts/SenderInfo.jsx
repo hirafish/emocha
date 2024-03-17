2613ad2a74510412c554020b583e9fafb661e5ed
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-17 19:07:44 +0900

    SenderInfoに表示例を追加

--- frontend/src/components/main/chat/parts/SenderInfo.jsx
+++ frontend/src/components/main/chat/parts/SenderInfo.jsx
@@ -2,7 +2,7 @@ import UserIcon from "../../globalParts/UserIcon";
 
 const SenderInfo=()=>{
     return (
-        <div className="absolute z-30 top-0 bottom-8 left-0 right-0 m-auto w-72 h-40 bg-white rounded-lg bg-opacity-85">
+        <div className="absolute z-30 top-0 bottom-8 left-0 right-0 m-auto w-72 h-40 bg-white rounded-lg bg-opacity-85 shadow">
             <div className="flex items-center justify-end">
                 <span className="p-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="w-4 h-4">
@@ -11,10 +11,33 @@ const SenderInfo=()=>{
                     </svg>
                 </span>
             </div>
-            <div>
-                <span className="w-10 h-10">
-                    <UserIcon image={"Ghost"} color={"Purple"} size={10} />
-                </span>
+            <div className="px-4 pb-2">
+                <div className="flex items-center">
+                    <span className="w-10 h-10">
+                        <UserIcon image={"Ghost"} color={"Purple"} size={10} />
+                    </span>
+                    <span className="pl-3">Obake</span>
+                </div>
+                <div className="h-full pl-5 pr-4 pt-2">
+                    <div className="flex items-center">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4">
+                                {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                                <path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"/>
+                            </svg>
+                        </span>
+                        <span className="mx-3">English</span>
+                    </div>
+                    <div className="flex items-center pt-2">
+                        <span className=" cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="w-4 h-4">
+                                {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                                <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/>
+                            </svg>
+                        </span>
+                        <span className="mx-3 overflow-auto cursor-pointer">https://aaaaaaaaaaaaaaa.com</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
