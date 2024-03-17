c5b5219e7e00a4f240910ef155d4047f51f33ab8
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-17 18:23:53 +0900

    相手の情報を表示する場所を追加

---
+++ frontend/src/components/main/chat/parts/SenderInfo.jsx
@@ -0,0 +1,17 @@
+const SenderInfo=()=>{
+    return (
+        <div className="absolute z-30 top-0 bottom-8 left-0 right-0 m-auto w-72 h-40 bg-white rounded-lg bg-opacity-85">
+            <div className="flex items-center justify-end">
+                <span className="p-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="w-4 h-4">
+                        {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                    </svg>
+                </span>
+            </div>
+            <p>aaaa</p>
+        </div>
+    );
+};
+
+export default SenderInfo;
