28e4d0984b2d2b2cf9306243f18b88e75443d66a
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-19 19:55:40 +0900

    ナビゲーションをダークモードに対応

--- frontend/src/components/main/nav/parts/LinkButton.jsx
+++ frontend/src/components/main/nav/parts/LinkButton.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 const LinkButton=({svgIcon,pageName,to})=>{
     return (
-        <NavLink to={to} className="navLink text-gray-600 block py-4 px-12 border-l-4 hover:bg-gray-300 hover:text-black" >
-            <span className="inline-block align-text-bottom mr-2 w-4 h-4">
-                <img src={svgIcon} />
+        <NavLink to={to} className="navLink text-gray-600 block py-4 px-12 border-l-4 hover:bg-gray-300 hover:text-black dark:text-gray-100 dark:hover:bg-gray-900 dark:hover:border-gray-900 dark:hover:text-white dark:border-gray-800" >
+            <span className="inline-block align-text-bottom mr-2 w-4 h-4 dark:text-white">
+                <img src={svgIcon} className="dark:text-white" />
             </span>
             {pageName}
         </NavLink>
