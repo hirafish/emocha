e283be4e1b954c4247ec21c97793f7ee9c4be7eb
takoko55 <ai625292@gmail.com>
2024-03-23 01:41:24 +0900

    表示されてるスラングがどこの言語でどういう意味なのかを明示

--- frontend/src/components/main/chat/parts/SlangInfo.jsx
+++ frontend/src/components/main/chat/parts/SlangInfo.jsx
@@ -13,7 +13,7 @@ const SlangInfo=({slangsList})=>{
                     <span className="w-full flex flex-col justify-center items-center">
                         {slangsList.map((slangObj,index)=>{
                             return(
-                            <p key={index} className="h-4 flex items-center dark:text-white"><DisplayEmojis emojiShortcodesList={[slangObj.shortcodes]} /> {userSettings.language === "English"?"means":"は"} " {slangObj.meaning} " {userSettings.language === "English"?"":"という意味です"}</p>
+                            <p key={index} className="h-4 flex items-center dark:text-white"><DisplayEmojis emojiShortcodesList={[slangObj.shortcodes]} /> {userSettings.language === "English"?"means":"は、英語で"} " {slangObj.meaning} " {userSettings.language === "English"?"in Japanese.":"という意味です"}</p>
                             )
                         })}
                     </span>
