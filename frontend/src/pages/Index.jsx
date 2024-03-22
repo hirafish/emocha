1a193940ce614a5f3eda58e35db5bacc6ea9f03f
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-22 10:08:09 +0900

    背景を追加

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -16,6 +16,7 @@ import { LanguagesCatalogContext } from "../components/providers/LanguagesCatalo
 
 
 
+
 const IndexPage = () => {
   const { userSettings } = useContext(UserSettingsContext);
 
@@ -184,7 +185,7 @@ const IndexPage = () => {
     };
 
   return (
-    <div className="main-body container m-auto w-11/12 h-full flex flex-col overflow-auto  bg-gray-100">
+    <div className="main flex-1 flex flex-col min-h-0 dark:bg-gray-900">
         <header className="sticky top-0 w-full bg-white shadow flex justify-between dark:bg-gray-900 dark:shadow-gray-100">
             <span className="text-xl xl:text-3xl font-medium mx-8 dark:text-white flex items-center">
                 <LogoSet />
@@ -211,6 +212,31 @@ const IndexPage = () => {
 
 
       <div className="flex-1 w-full h-full">
+
+      <section style={{background:"url(/bgImage.svg)"}} class="bg-white w-full h-screen dark:bg-gray-900 grid">
+
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 col-start-1 row-start-1 my-auto">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Get started
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <a href="#" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70">
+                Learn more
+            </a>  
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
         <div className="lg:flex m-auto">
           <div class="flex flex-col flex-1 justify-center lg_justify-start px-4 sm_px-6 py-12 lg_pt-36 lg_flex-none lg_px-20 xl_px-24">
             <div class="md_h-3/4 w-full max-w-sm mx-auto lg_w-96">
