42f31d5af90bb56ef620f899ceab6cff1b802e37
ayakakawabe <kawabe.ayaka@icloud.com>
2024-03-22 13:00:22 +0900

    ダークモード対応

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -212,7 +212,6 @@ const IndexPage = () => {
         </header>
         <div className="flex-1 w-full h-full">
             <section style={{background:"url(/bgImage.svg)"}} className="bg-white w-full h-[94svh] dark:bg-gray-900 grid">
-
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 col-start-1 row-start-1 my-auto rounded bg-white dark:bg-gray-900 dark:bg-opacity-50 bg-opacity-80">
                     <div className="flex justify-center items-center">
                         <span className="flex justify-center bg-white dark:bg-gray-900 p-4 rounded mb-6 md:mb-3">
@@ -227,19 +226,18 @@ const IndexPage = () => {
                     <h2 className="mb-6 md:mb-4 text-xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{language==="English"?<span>Let's start an emoji-only chat !</span>:<span>絵文字だけのチャットを始めよう！</span>}</h2>
                     <p className="mb-10 md:mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">{language==="English"?<span>Even if there are no words,<br className="md:hidden"/> we can communicate with each other.</span>:<span>言葉がなくても、心が通じる。</span>}</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                        <AnchorLink href="#SignUp" offset="170" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        <AnchorLink href="#SignUp" offset="150" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                             {language==="English"?"Get started":"サインアップ"}
                         </AnchorLink>
-                        <AnchorLink href="#LogIn" offset="170" className="py-3 px-5 sm:ms-8 text-base font-medium text-gray-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 ">
+                        <AnchorLink href="#LogIn" offset="150" className="py-3 px-5 sm:ms-8 text-base font-medium text-gray-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 ">
                             {language==="English"?"Log in":"ログイン"}
                         </AnchorLink>  
                     </div>
                 </div>
             </section>
-
-            <div className="h-16 bg-white"/>
-            <div className="m-auto py-10 px-2 bg-green-50 shadow-xl shadow-green-50">
-                <div id="LogIn" className="flex flex-col justify-center items-center px-4 md:px-10 py-12 my-40 bg-white dark:bg-gray-900 rounded-md mx-auto md:w-3/6 xl:w-2/5">
+            <div className="h-16 bg-white dark:bg-gray-900"/>
+            <div className="m-auto py-10 px-2 bg-green-50 shadow-xl shadow-green-50 dark:bg-emerald-950">
+                <div id="LogIn" className="flex flex-col justify-center items-center px-4 md:px-10 py-12 my-40 bg-white dark:bg-gray-900 rounded-lg mx-auto md:w-3/6 xl:w-2/5">
                     <h2 className="my-8 text-3xl font-extrabold text-gray-900 text-center dark:text-white">
                     Log in to your account
                     </h2>
@@ -293,8 +291,8 @@ const IndexPage = () => {
                     </p> */}
                     </form>          
                 </div>
-                <div className="border-b-2 border-b-white"/>
-                <div id="SignUp" className="flex flex-col justify-center items-center px-4 md:px-10 py-12 my-40 bg-white dark:bg-gray-900 rounded-md mx-auto md:w-3/6 xl:w-2/5">
+                <div className="border-b-8 border-b-white dark:border-b-gray-900"/>
+                <div id="SignUp" className="flex flex-col justify-center items-center px-4 md:px-10 py-12 my-40 bg-white dark:bg-gray-900 rounded-lg mx-auto md:w-3/6 xl:w-2/5">
                     <h2 className="my-8 text-3xl font-extrabold text-gray-900 text-center dark:text-white">
                     Sign up to your account
                     </h2>
@@ -348,18 +346,18 @@ const IndexPage = () => {
             </div>
             </div>
             <footer className="">
-            <div className="bg-gray-50">
+            <div className="bg-gray-50 dark:bg-gray-900">
                 <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-                    <a href="./" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                    <a href="./" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-white">
                         <img src="/emochaLogo.png" className="w-10 h-10 text-white rounded"/>
                     </a>
-                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">&copy; 2024 emocha — 
-                        <a href="https://github.com/hirafish/emocha" rel="noopener noreferrer" translate="no" className="text-gray-600 ml-1" target="_blank">
+                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4 dark:text-white">&copy; 2024 emocha — 
+                        <a href="https://github.com/hirafish/emocha" rel="noopener noreferrer" translate="no" className="text-gray-600 ml-1 dark:text-white" target="_blank">
                             りんごらてオバケ 🍏☕👻
                         </a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a href="https://github.com/hirafish/emocha" target="_blank" className="text-gray-500">
+                        <a href="https://github.com/hirafish/emocha" target="_blank" className="text-gray-500 dark:text-white">
                             <svg fill="currentColor" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" >
                                 <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="currentColor"/>
                             </svg>
